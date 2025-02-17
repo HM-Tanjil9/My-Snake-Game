fc9b60d9a1b5a3b04393e200d47cd6862d84b2e6
HM-Tanjil9 <rtanjil0@gmail.com>
2025-02-18 00:16:55 +0600

    Draw food & snake

---
+++ README.md
@@ -0,0 +1,6 @@
+Snake Game
+
+A Snake Game build using HTML,CSS & Javascript.
+About
+
+This is a simple Snake Game implemented using HTML, CSS, and JavaScript. It's a classic game where the player controls a snake that moves around the screen, eating food to grow longer while avoiding collisions with itself and the walls.
--- script.js
+++ script.js
@@ -4,12 +4,41 @@ document.addEventListener('DOMContentLoaded', function() {
     const cellSize = 20;
     let score = 0; //score of the game 
     let gameStarted = false; //game status
-    let food = {x: 300, y: 200}; // {x: 15*20, y: 10*20} //cell coordinate -> pixels
+    let food = {x: 300, y: 200}; // {x: 15*20, y: 10*20} //cell coordinate -> top & left pixels
     let snake = [{x: 160, y: 200}, {x: 140, y: 200}, {x: 120, y: 200}];
+
+    // Draw Div function
+    function drawDiv(x, y, className) {
+        const divElement = document.createElement('div');
+        divElement.classList.add(className);
+        divElement.style.top = `${y}px`;
+        divElement.style.left = `${x}px`;
+        return divElement;
+    }
+
+    // Draw food & snake
+    function drawFoodAndSnake() {
+        gameArena.innerHTML = ''; // clear the game arena
+
+        // Snake element
+        snake.forEach((snakeCell) => {
+            const snakeElement = drawDiv(snakeCell.x, snakeCell.y, 'snake');
+            gameArena.appendChild(snakeElement);
+        })
+
+        // Food element
+        const foodElement = drawDiv(food.x, food.y, 'food');
+        gameArena.appendChild(foodElement);
+
+    }
 
     // Run game
     function runGame() {
-        
+        if(!gameStarted) {
+            gameStarted = true;
+            drawFoodAndSnake(); // Make food & snake
+            // gameLoop(); TODO: Implement game loop
+        }
     }
 
     // Initiate the game
