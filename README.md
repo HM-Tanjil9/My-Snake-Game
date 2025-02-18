4addaa5f042b272a91e2e373a80c7582a8ae07d8
HM-Tanjil9 <rtanjil0@gmail.com>
2025-02-18 09:43:40 +0600

    Added food movement logic

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
-Snake Game
+# Snake Game
+
 A Snake Game build using HTML,CSS & Javascript.
 
-About
+# About
+
 This is a simple Snake Game implemented using HTML, CSS, and JavaScript. It's a classic game where the player controls a snake that moves around the screen, eating food to grow longer while avoiding collisions with itself and the walls.
+
+# How to play
+
+⦿ Use the arrow keys (Up, Down, Left, Right) to control the direction of the snake.
+
+⦿ Eat the food (represented by red squares) to grow longer.
+
+⦿ The game speed increases as the snake grows longer, making it more challenging.
+
+⦿ If running into the walls or the snake's own body, as this will end the game.
+
+# Extra Featues
+
+⦿ Increase Speed of the Snake everytime its eats a food.
+
+⦿ Can't move in the opposite direction in which the snake moving etc...
+
+# Prerequisites
+
+⦿ HTML: For structuring the game layout.
+
+⦿ CSS: For styling the game elements and layout.
+
+⦿ JavaScript: For implementing the game logic and functionality.
+
+# Author
+
+Tanjil Rahman
--- script.js
+++ script.js
@@ -8,8 +8,19 @@ document.addEventListener('DOMContentLoaded', function() {
     let snake = [{x: 160, y: 200}, {x: 140, y: 200}, {x: 120, y: 200}]; // [head, body, ... , tail]
     let dx = cellSize; // Snake Change direction of X
     let dy = 0; // Snake Change direction of Y
+    let intervalId;
+    let gameSpeed = 200;
 
+    // Move food
+    function moveFood() {
+        let newX, newY;
+        do {
+            newX = Math.floor(Math.random() * 30) * cellSize;
+            newY = Math.floor(Math.random() * 30) * cellSize;
+        } while(snake.some(snakeCell => snakeCell.x === newX && snakeCell.y === newY));
 
+        food = {x: newX, y: newY};
+    }
     // Update snake 
     function updateSnake () {
         // Create new head
@@ -19,7 +30,12 @@ document.addEventListener('DOMContentLoaded', function() {
         // Check collision with food
         if(newHead.x === food.x && newHead.y === food.y) {
             score += 10; // Update score
-            // TODO: Move food item
+            moveFood(); // Move food item
+            if(gameSpeed > 50) {
+                clearInterval(intervalId);
+                gameSpeed -= 10; // change game speeds
+                gameLoop();
+            }
         } else {
             snake.pop() // Remove tail
         }
@@ -49,6 +65,12 @@ document.addEventListener('DOMContentLoaded', function() {
         
     }
 
+    // Draw score board
+    function drawScoreBoard() {
+        const scoreBoard = document.getElementById('score-board');
+        scoreBoard.textContent = `Score: ${score}`;
+    }
+
     // Draw Div function
     function drawDiv(x, y, className) {
         const divElement = document.createElement('div');
@@ -74,12 +96,37 @@ document.addEventListener('DOMContentLoaded', function() {
 
     }
 
+    // Is game over
+    function isGameOver() {
+        // Snake collision check
+        for(let i=1; i<snake.length; i++) {
+            if(snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+                return true;
+            }
+        }
+
+        // Wall collision check
+        const hitLeftWall = snake[0].x < 0;
+        const hitRightWall = snake[0].x > arenaSize - cellSize;
+        const hitTopWall = snake[0].y < 0;
+        const hitDownWall = snake[0].y > arenaSize - cellSize;
+        return hitLeftWall || hitRightWall || hitTopWall || hitDownWall;
+    }
+
     // Game loop 
     function gameLoop() {
-        setInterval(() => {
+        intervalId = setInterval(() => {
+            
+            if(isGameOver()) {
+                clearInterval(intervalId);
+                gameStarted = false;
+                alert("Game is over" + "\n" + "Your score: " + score)
+                return;
+            }
             updateSnake();
             drawFoodAndSnake();
-        }, 200);
+            drawScoreBoard();
+        }, gameSpeed);
     }
 
     // Run game
