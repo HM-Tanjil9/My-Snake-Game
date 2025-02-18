6e10f650fac91f8a8a3f5612b481e3ea9e562d71
HM-Tanjil9 <rtanjil0@gmail.com>
2025-02-18 07:46:39 +0600

    Added snake movement logic

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 Snake Game
-
 A Snake Game build using HTML,CSS & Javascript.
-About
 
+About
 This is a simple Snake Game implemented using HTML, CSS, and JavaScript. It's a classic game where the player controls a snake that moves around the screen, eating food to grow longer while avoiding collisions with itself and the walls.
--- script.js
+++ script.js
@@ -5,7 +5,49 @@ document.addEventListener('DOMContentLoaded', function() {
     let score = 0; //score of the game 
     let gameStarted = false; //game status
     let food = {x: 300, y: 200}; // {x: 15*20, y: 10*20} //cell coordinate -> top & left pixels
-    let snake = [{x: 160, y: 200}, {x: 140, y: 200}, {x: 120, y: 200}];
+    let snake = [{x: 160, y: 200}, {x: 140, y: 200}, {x: 120, y: 200}]; // [head, body, ... , tail]
+    let dx = cellSize; // Snake Change direction of X
+    let dy = 0; // Snake Change direction of Y
+
+
+    // Update snake 
+    function updateSnake () {
+        // Create new head
+        const newHead = {x: snake[0].x + dx, y: snake[0].y + dy};
+        snake.unshift(newHead); // Added new head
+
+        // Check collision with food
+        if(newHead.x === food.x && newHead.y === food.y) {
+            score += 10; // Update score
+            // TODO: Move food item
+        } else {
+            snake.pop() // Remove tail
+        }
+    }
+
+    // Snake change direction 
+    function changeDirection (e) {
+        console.log('key pressed', e);
+        const isGoingDown = dy === cellSize;
+        const isGoingUp = dy === -cellSize;
+        const isGoingRight = dx === cellSize;
+        const isGoingLeft = dx === -cellSize;
+
+        if(e.key === 'ArrowUp' && !isGoingDown) {
+            dx = 0;
+            dy = -cellSize;
+        } else if(e.key === 'ArrowDown' && !isGoingUp) {
+            dx = 0;
+            dy = cellSize;
+        } else if(e.key === 'ArrowLeft' && !isGoingRight) {
+            dx = -cellSize;
+            dy = 0;
+        } else if(e.key === 'ArrowRight' && !isGoingLeft) {
+            dx = cellSize;
+            dy = 0;
+        }
+        
+    }
 
     // Draw Div function
     function drawDiv(x, y, className) {
@@ -32,12 +74,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
     }
 
+    // Game loop 
+    function gameLoop() {
+        setInterval(() => {
+            updateSnake();
+            drawFoodAndSnake();
+        }, 200);
+    }
+
     // Run game
     function runGame() {
         if(!gameStarted) {
             gameStarted = true;
             drawFoodAndSnake(); // Make food & snake
-            // gameLoop(); TODO: Implement game loop
+            document.addEventListener('keydown', changeDirection)
+            gameLoop(); // Implement game loop
         }
     }
 
